c8c7cb45796e56bfaf56e763cfe749cda64ce94e
Pradipa Y <merakayamsstraat@gmail.com>
2025-10-23 15:26:13 +0700

    membuat variabel gambar di row di aktifitasPertama di UIpertama

--- app/src/main/java/com/example/myuifirstpertamagueh/UIpertama.kt
+++ app/src/main/java/com/example/myuifirstpertamagueh/UIpertama.kt
@@ -50,6 +50,14 @@ fun ActivitasPertama(modifier: Modifier){
             )
         ){
             Row (){
+                val gambar = painterResource(id = R.drawable.huto)
+                Image(
+                    painter = gambar,
+                    contentDescription = null,
+                    modifier = Modifier
+                        .size(size = 100.dp)
+                        .padding(all = 5.dp)
+                )
 
             }
         }
