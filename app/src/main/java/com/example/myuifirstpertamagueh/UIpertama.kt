715a64688423a19c8713a9dad0d7b60a531da91c
Pradipa Y <merakayamsstraat@gmail.com>
2025-10-24 18:35:07 +0700

    menambah text 1 di column di row di card di box ketiga di UIpertama.kt

--- app/src/main/java/com/example/myuifirstpertamagueh/UIpertama.kt
+++ app/src/main/java/com/example/myuifirstpertamagueh/UIpertama.kt
@@ -189,6 +189,13 @@ fun ActivitasPertama(modifier: Modifier = Modifier) {
                     )
                     Spacer(modifier = Modifier.width(30.dp))
                     Column {
+                        Text(
+                            text = stringResource(id = R.string.nama3),
+                            fontSize = 28.sp,
+                            fontFamily = SFProDisplay,
+                            color = Color.White,
+                            modifier = Modifier.padding(top = 10.dp)
+                        )
 
                     }
 
